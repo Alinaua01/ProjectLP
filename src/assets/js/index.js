c6fcfd5957d3ee59f447217f5a767c80cd763499
Alina <linachamlay@gmail.com>
2022-12-12 17:51:32 +0300

    smth

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,14 +1,1 @@
-
-
-document.querySelector('#btn1').onclick = () => {
-    document.querySelector('.home-page__modal-container').classList.add('is-visible')
-}
-
-document.querySelector('.home-page__modal-close').onclick = () => {
-    document.querySelector('.home-page__modal-container').classList.remove('is-visible')
-}
-
-
-
-
-
+console.log("test");
